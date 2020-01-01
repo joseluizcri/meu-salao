f86a2fd5f5fc748183a19e1a167d9b68933295c2
José Luiz <junior.joseluizrosa@gmail.com>
2020-01-01 20:38:17 -0200

    Primeiro commit

---
+++ src/main/java/br/com/atualizesistemas/meusalao/MeuSalao/MeuSalaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.atualizesistemas.meusalao.MeuSalao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuSalaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuSalaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/atualizesistemas/meusalao/MeuSalao/MeuSalaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atualizesistemas.meusalao.MeuSalao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuSalaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
